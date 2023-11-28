894f75ff96c51224c7bd0966046bbdaa8f7b666e
João Pedro E. Silveira <108689656+jpsilveira11@users.noreply.github.com>
2023-11-28 10:59:10 -0300

    opening cards system

--- src/styles/engine.js
+++ src/styles/engine.js
@@ -16,5 +16,21 @@ for(let index=0; index<emojis.length;index++){
     let box=document.createElement("div");
     box.className="item";
     box.innerHTML=shuffle[index];
+    box.onclick=handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function matched(){
+    
+}
+
+function handleClick(){
+    if(openCards<2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length==2){
+        setTimeout(matched,500)
+
+    }
 }
